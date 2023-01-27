3c7c9b75d3a74c6ee6251d1e7895078d15a27b4b
moybadajoz <moisesbadajoz36@gmail.com>
2023-01-27 10:50:39 -0600

    iniciamos proyecto

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const PORT = 5000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>Hola Mundo!</h1>')
+})
+
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando: ${PORT}`)
+    //console.log('Servidor escuchando: ' + PORT)
+})
